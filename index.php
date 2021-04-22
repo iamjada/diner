366a5183c72dae0aa803b53bdfc4881a06dcd3cb
iamjada <jsenebouttarath@mail.greenriver.edu>
2021-04-22 02:14:35 -0400

    added home page

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+// This is my controller for the diner project
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require autoload file
+require_once('vendor/autoload.php');
+
+// instantiate fat-free
+$f3 = Base::instance();
+
+// define routes
+$f3->route('GET /', function(){
+
+    // Display the home page
+    $view = new Template(); // instantiate view object
+    echo $view->render('views/home.html'); // using view object to display the view page
+
+
+});
+
+// run fat-free
+$f3->run();
